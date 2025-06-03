9b26f9c03d573cbfa6e3902f0d1a7bd9529d7375
Kpei Sylvester <kpeis695@gmail.com>
2025-06-03 15:53:53 -0400

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -61,7 +61,7 @@ function createExtendedUndoModal(emailData, sendButton) {
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.5);
-        z-index: 10000;
+        z-index: 999999;
         display: flex;
         align-items: center;
         justify-content: center;
